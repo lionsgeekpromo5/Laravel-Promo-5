68731e1e4198b74349be46a92f9eae27713d7af8
lionsgeekpromo5 <sellingjewelery62@gmail.com>
2026-02-25 15:20:41 +0000

    second commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //* function or methods of the class
+
+    //? function that returns home view
+    public function index(){
+        return view('home');
+    }
+
+
+
+    
+
+
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/home', function () {
+//     return view('home');
+// } );
+Route::get('/home',  [HomeController::class, 'index'])->name('home.index');
